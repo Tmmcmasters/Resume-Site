c8f3be5a3d3c226baffc38bbe5f84d3fc6698686
tmmcmasters <tmmcmasters@gmail.com>
2026-01-26 21:07:00 -0500

    Handling the shadcn

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,13 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+
   vite: {
     plugins: [tailwindcss()],
+  },
+
+  modules: ["shadcn-nuxt"],
+  shadcn: {
+    componentDir: "@/components/ui",
   },
 });

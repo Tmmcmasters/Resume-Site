875485f8d58dfc6982def0ee09f8b2f968e7e6ec
tmmcmasters <tmmcmasters@gmail.com>
2026-01-26 21:25:18 -0500

    Starting to create layout and everything

---
+++ app/page/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+useHead({
+    title: 'Tim McMasters',
+    meta: [
+        { name: 'description', content: 'Tim McMasters - Software Engineer' },
+    ]
+})
+</script>
+
+<template>
+    <div>
+
+    </div>
+</template>
+
+
+<style scoped>
+
+</style>

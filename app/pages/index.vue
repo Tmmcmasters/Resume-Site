12749291c097f4b46b80726d0257f54470763f03
tmmcmasters <tmmcmasters@gmail.com>
2026-03-14 22:48:31 -0400

    I was able to get Tailwindcss to work

---
+++ app/pages/index.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+useHead({
+    title: 'Tim McMasters',
+    meta: [
+        { name: 'description', content: 'Tim McMasters - Software Engineer' },
+    ]
+})
+
+definePageMeta({
+    layout: 'resume'
+})
+
+const data = [
+  {
+    id: "version1.0",
+    label: "Version 1.0",
+  },
+  {
+    id: "version2.0",
+    label: "Version 2.0",
+  },
+
+  {
+    id: "version3.0",
+    label: "Version 3.0",
+  },
+  {
+    id: "version4.0",
+    label: "Version 4.0",
+  },
+
+  {
+    id: "version5.0",
+    label: "Version 5.0",
+  },
+  {
+    id: "version6.0",
+    label: "Version 6.0",
+  },
+];
+</script>
+
+<template>
+  <div class="h-fit w-full">
+    <p class="">Testing custom font</p>
+
+  </div>
+</template>
+
+<script scoped></script>
+
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/image']
-})
+  css: ["assets/css/tailwind.css"],
+  vite: {
+    // @ts-expect-error - Vite version mismatch between @tailwindcss/vite and Nuxt's internal Vite
+    plugins: [tailwindcss()],
+  },
+  modules: ["@nuxt/eslint", "@nuxt/image"],
+});

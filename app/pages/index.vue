ddf8599ef28af50c6a706dcb43f85ce2a5903f86
tmmcmasters <tmmcmasters@gmail.com>
2026-03-29 18:12:55 -0400

    Starting to build the top section

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -11,8 +11,14 @@ definePageMeta({
 
 <template>
   <div class="h-full w-full flex justify-center items-center relative">
-    <div class="flex flex-col justify-center items-center">
-      <div></div>
+    <div class="flex flex-col justify-center items-center py-21.25">
+      <div
+        class="w-full max-w-275.25 h-fit flex justify-center items-center gap-x-10.5"
+      >
+        <!-- Image of Me -->
+        <NuxtImg src="/imgs/image-of-me.png" class="h-25" />
+        <h1 class="font-crimson text-8xl text-center">Tim McMasters</h1>
+      </div>
     </div>
   </div>
 </template>
